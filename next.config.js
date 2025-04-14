5c9dede50a3bdc0770c199031ee47f9c4d26f8b8
Evan Pradipta Hardinatha <84654936+kleopasevan@users.noreply.github.com>
2025-04-15 01:36:03 +0700

    Update image conf for static

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  // Configure images for static export
+  images: {
+    loader: 'akamai', // Use 'akamai' or 'imgix', 'cloudinary' if you use those services
+    path: '', // Use empty string or the base URL if needed by loader/CDN
+    // Ensure domains for external images are listed if necessary
+    // domains: ['example.com'],
+  },
 };
 
 module.exports = nextConfig; 
