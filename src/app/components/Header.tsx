e5d53f6604ca83c601a72bd43c6f91723b709253
MTharreq An Nahl <mtharreq@gmail.com>
2025-05-31 19:15:48 +0700

    feat: update package dependencies and enhance Header component with responsive Sheet dialog

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 
 export default function Header() {
   const navItems = [
@@ -10,34 +20,70 @@ export default function Header() {
   ];
 
   return (
-    <header className="sticky top-0 z-10 flex items-center justify-between border-b border-[#F2F2F2] bg-white px-8 pb-1 pt-2">
-      <div className="flex items-center gap-4">
-        <Image
-          src="/logo.png"
-          alt="VIXTERO Logo"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="h-[60px] w-auto hover:cursor-pointer"
-        />
-        <nav>
-          <ul className="flex font-dongle text-2xl font-light">
-            {navItems.map((item) => (
-              <li key={item.href}>
-                <Link
-                  href={item.href}
-                  className="p-4 transition duration-300 hover:font-medium"
-                >
-                  {item.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <header className="sticky top-0 z-10 border-b border-[#F2F2F2] bg-white">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 pt-2 xl:px-0">
+        <div className="flex items-center gap-4">
+          <Image
+            src="/logo.png"
+            alt="VIXTERO Logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-[60px] w-auto hover:cursor-pointer"
+          />
+          <nav>
+            <ul className="hidden font-dongle text-2xl font-light md:flex">
+              {navItems.map((item) => (
+                <li key={item.href}>
+                  <Link
+                    href={item.href}
+                    className="p-4 transition duration-300 hover:font-medium"
+                  >
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <button className="hidden rounded-lg border bg-[#004766] px-4 py-2 text-sm text-white transition duration-300 hover:bg-[#00476695] md:block">
+          Book a Demo
+        </button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline" className="md:hidden">
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle></SheetTitle>
+              <SheetDescription></SheetDescription>
+            </SheetHeader>
+
+            <nav>
+              <ul className="space-y-2 font-dongle text-2xl font-light">
+                {navItems.map((item) => (
+                  <li key={item.href}>
+                    <Link href={item.href}>
+                      <div className="w-full transition duration-300 hover:font-medium">
+                        {item.label}
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+
+            <Button
+              className="mt-2 w-full bg-[#004766] hover:bg-[#004766] hover:brightness-90"
+              type="submit"
+            >
+              Book a Demo
+            </Button>
+          </SheetContent>
+        </Sheet>
       </div>
-      <button className="rounded-lg border bg-[#004766] px-4 py-2 text-sm text-white transition duration-300 hover:bg-[#00476695]">
-        Book a Demo
-      </button>
     </header>
   );
 }
--- src/app/vixborder/page.tsx
+++ src/app/vixborder/page.tsx
@@ -1,115 +1,135 @@
 import Image from "next/image";
 import ContactUs from "../components/ContactUs";
 
-export default function Vixborder() {
+export default function Page() {
   return (
     <>
-      <section>
-        <div className="bg-white px-6 py-12 text-center">
-          <div className="mx-auto flex max-w-7xl items-center gap-8">
-            <div className="flex-1">
-              <div className="flex w-fit items-center gap-1 rounded-full p-[2px] pr-2 text-xs text-[#006B99] shadow">
-                <div className="rounded-full bg-gradient-to-r from-[#99E1FF] via-[#E5F8FF] to-[#CCF0FF] p-2 py-1 text-sm font-semibold text-[#006B99]">
-                  VIXBORDER
-                </div>{" "}
-                by VIXTERO
-              </div>
-              <h1 className="mt-2 text-start text-4xl font-bold text-[#00648F]">
-                <span className="text-[#004766]"> Automate</span> your
-                Export-Import Document in Seconds
-              </h1>
-              <p className="text-start text-base text-[#646464]">
-                Extract document data with AI, ensure validation, and send
-                declarations straight to CEISA 4.0.
-              </p>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-              <button className="rounded-lg bg-gradient-to-r from-[#99E1FF] via-[#E5F8FF] to-[#CCF0FF] p-4 font-semibold text-[#006B99]">
-                Request a Demo
-              </button>
-              <div className="mt-2 cursor-pointer text-[#006B99]">
-                See other products ▾
-              </div>
-            </div>
-          </div>
-          <div className="mx-auto mt-10 h-[600px] max-w-7xl rounded-md border p-8"></div>
-        </div>
-
-        <div className="mx-auto grid max-w-7xl grid-cols-2 place-items-center gap-8 bg-[#F0FAFF] px-6 py-12">
-          <div className="h-fit">
-            <h2 className="mb-2 text-xl font-bold text-[#00648F]">
-              AI-Powered OCR: From Documents to Structured Forms
-            </h2>
-            <p className="mb-4 text-[#646464]">
-              Extracts data from Commercial Invoices, Packing Lists, Airway
-              Bills (AWB), and Bills of Lading (BL) with up to 99% accuracy,
-              converting unstructured documents into usable form data
-              automatically.
-            </p>
-          </div>
-          <Image
-            src="/img/vixborder/feature-1.png"
-            alt="OCR Example"
-            width={400}
-            height={200}
-            className="h-auto w-full"
-          />
-
-          <Image
-            src="/img/vixborder/feature-2.png"
-            alt="OCR Example"
-            width={400}
-            height={200}
-            className="h-auto w-full"
-          />
-          <div>
-            <h2 className="mb-2 text-xl font-bold text-[#00648F]">
-              Automated HS Code & Lartas Validation
-            </h2>
-            <p className="mb-4 text-[#646464]">
-              Automatically identifies HS Codes and checks for Lartas
-              (Prohibited & Restricted Items) regulations using up-to-date
-              official databases.
-            </p>
-          </div>
-
-          <div>
-            <h2 className="mb-2 text-xl font-bold text-[#00648F]">
-              Internal Approval Workflow
-            </h2>
-            <p className="mb-4 text-[#646464]">
-              Provides structured document approval flows with Maker, Checker,
-              and Approver roles, including real-time notifications for faster
-              review and decision-making.
-            </p>
-          </div>
-          <Image
-            src="/img/vixborder/feature-3.png"
-            alt="OCR Example"
-            width={400}
-            height={200}
-            className="h-auto w-full"
-          />
-
-          <Image
-            src="/img/vixborder/feature-4.png"
-            alt="OCR Example"
-            width={400}
-            height={200}
-            className="h-auto w-full"
-          />
-          <div>
-            <h2 className="mb-2 text-xl font-bold text-[#00648F]">
-              Automated & Dynamic Cost Calculation
-            </h2>
-            <p className="mb-4 text-[#646464]">
-              Automatically calculates duties, taxes (VAT, PPH), and logistics
-              costs based on document data and current regulations.
-            </p>
-          </div>
-        </div>
-      </section>
+      <Hero />
+      <Features />
       <ContactUs />
     </>
+  );
+}
+
+function Hero() {
+  return (
+    <section className="bg-white px-3 py-12 text-center sm:px-6">
+      <div className="mx-auto max-w-7xl items-center gap-8 md:flex">
+        <div className="flex-1">
+          <div className="flex w-fit items-center gap-1 place-self-center rounded-full p-[2px] pr-2 text-xs text-[#006B99] shadow md:place-self-start">
+            <div className="rounded-full bg-gradient-to-r from-[#99E1FF] via-[#E5F8FF] to-[#CCF0FF] p-2 py-1 text-sm font-semibold text-[#006B99]">
+              VIXBORDER
+            </div>{" "}
+            by VIXTERO
+          </div>
+
+          <h1 className="mt-2 text-pretty text-center text-3xl font-bold text-[#00648F] md:text-start">
+            <span className="text-[#004766]"> Automate</span> your Export-Import
+            Document in Seconds
+          </h1>
+          <p className="text-pretty text-center text-sm text-[#646464] md:text-start md:text-base">
+            Extract document data with AI, ensure validation, and send
+            declarations straight to CEISA 4.0.
+          </p>
+        </div>
+        <div className="mt-8 flex flex-col items-center justify-center md:mt-0">
+          <button className="rounded-lg bg-gradient-to-r from-[#99E1FF] via-[#E5F8FF] to-[#CCF0FF] px-4 py-2 font-semibold text-[#006B99] md:p-4">
+            Request a Demo
+          </button>
+          <div className="mt-2 cursor-pointer text-base text-[#006B99]">
+            See other products ▾
+          </div>
+        </div>
+      </div>
+
+      {/* HERO VIDEOS */}
+      <div className="mx-auto mt-10 max-w-7xl overflow-hidden rounded-lg border">
+        <video autoPlay muted loop playsInline className="background-video">
+          <source
+            src="https://d1gt37kdnsmaxt.cloudfront.net/vixborder-product-demo.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </section>
+  );
+}
+
+function Features() {
+  return (
+    <section className="bg-[#F0FAFF]">
+      <div className="mx-auto grid max-w-7xl grid-cols-1 place-items-center gap-2 px-6 py-12 md:grid-cols-2 md:gap-8">
+        <Image
+          src="/img/vixborder/feature-1.png"
+          alt="OCR Example"
+          width={400}
+          height={200}
+          className="order-1 h-auto w-full md:order-2"
+        />
+        <div className="order-2 mb-8 md:order-1 md:mb-0">
+          <h2 className="mb-2 text-xl font-bold text-[#00648F]">
+            AI-Powered OCR: From Documents to Structured Forms
+          </h2>
+          <p className="mb-4 text-[#646464]">
+            Extracts data from Commercial Invoices, Packing Lists, Airway Bills
+            (AWB), and Bills of Lading (BL) with up to 99% accuracy, converting
+            unstructured documents into usable form data automatically.
+          </p>
+        </div>
+
+        <Image
+          src="/img/vixborder/feature-2.png"
+          alt="OCR Example"
+          width={400}
+          height={200}
+          className="order-3 h-auto w-full"
+        />
+        <div className="order-4 mb-8 md:mb-0">
+          <h2 className="mb-2 text-xl font-bold text-[#00648F]">
+            Automated HS Code & Lartas Validation
+          </h2>
+          <p className="mb-4 text-[#646464]">
+            Automatically identifies HS Codes and checks for Lartas (Prohibited
+            & Restricted Items) regulations using up-to-date official databases.
+          </p>
+        </div>
+
+        <Image
+          src="/img/vixborder/feature-3.png"
+          alt="OCR Example"
+          width={400}
+          height={200}
+          className="order-5 h-auto w-full md:order-6"
+        />
+        <div className="order-6 mb-8 md:order-5 md:mb-0">
+          <h2 className="mb-2 text-xl font-bold text-[#00648F]">
+            Internal Approval Workflow
+          </h2>
+          <p className="mb-4 text-[#646464]">
+            Provides structured document approval flows with Maker, Checker, and
+            Approver roles, including real-time notifications for faster review
+            and decision-making.
+          </p>
+        </div>
+
+        <Image
+          src="/img/vixborder/feature-4.png"
+          alt="OCR Example"
+          width={400}
+          height={200}
+          className="order-7 h-auto w-full"
+        />
+        <div className="order-8 mb-8 md:mb-0">
+          <h2 className="mb-2 text-xl font-bold text-[#00648F]">
+            Automated & Dynamic Cost Calculation
+          </h2>
+          <p className="mb-4 text-[#646464]">
+            Automatically calculates duties, taxes (VAT, PPH), and logistics
+            costs based on document data and current regulations.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
