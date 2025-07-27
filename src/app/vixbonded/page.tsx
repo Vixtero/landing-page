3ac141e1481d290a178997f46008dd06a1b9a3b0
MTharreq An Nahl <mtharreq@gmail.com>
2025-07-27 19:05:05 +0700

    feat(Vixbonded, Vixfreight, Vixlite): update pages with 'Coming Soon' alerts, descriptions, and notification buttons

--- src/app/vixbonded/page.tsx
+++ src/app/vixbonded/page.tsx
@@ -1,15 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Page() {
   return (
-    <section className="flex flex-col text-4xl gap-4 font-archivo font-semibold text-gray-800 items-center justify-center h-dvh w-full">
-      VIXBONDED PAGE
-      <Link
-        href="/"
-        className="text-blue-500 hover:underline font-normal text-xl"
-      >
-        Back to home
-      </Link>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-12 lg:py-16">
+        <div
+          className="mb-7 inline-flex items-center justify-between rounded-full bg-gray-100 px-1 py-1 pr-4 text-sm text-gray-700 hover:bg-gray-200 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700"
+          role="alert"
+        >
+          <span className="mr-3 rounded-full bg-green-600 px-4 py-1.5 text-xs text-white">
+            Coming Soon
+          </span>
+          <span className="text-sm font-medium">
+            Vixbonded is launching soon!
+          </span>
+        </div>
+
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          Simplify Your Trade Operations with Vixbonded
+        </h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48">
+          Vixbonded is our upcoming lightweight solution for efficient
+          international trade management. Designed for speed, simplicity, and
+          success.
+        </p>
+
+        <div className="mb-8 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-x-4 sm:space-y-0 lg:mb-16">
+          <Button asChild className="bg-green-500 hover:bg-green-600">
+            <Link href="https://www.instagram.com/vixteropintar">
+              Notify Me <ArrowRight />
+            </Link>
+          </Button>
+          <Button asChild variant={"outline"}>
+            <Link href="/">Back to home</Link>
+          </Button>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
